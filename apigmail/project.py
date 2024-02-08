69fbf454ca318ec85ba486f014b5eb3f6a8dada9
Anitta Antony <98166149+Anitta-Antony@users.noreply.github.com>
2024-02-08 14:38:39 +0530

    Add files via upload

---
+++ apigmail/project.py
@@ -0,0 +1,127 @@
+from flask import Flask,redirect, url_for, request
+from Google import Create_Service
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from bs4 import BeautifulSoup
+
+CLIENT_SECRET_FILE = 'abc.json'
+API_NAME = 'gmail'
+API_VERSION = 'v1'
+SCOPES = ['https://mail.google.com/', 'https://www.googleapis.com/auth/gmail.readonly']
+
+"""def authenticate():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+    creds = flow.run_local_server(port=0)
+    return creds
+
+authenticate()"""
+app = Flask(__name__)
+def read_emails():
+    """
+    Retrieve emails from Gmail inbox
+    """
+   
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    # Call the Gmail API to retrieve emails
+    results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        return 'No messages found.'
+    else:
+        print("Messages:")
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            message_data = msg['payload']['headers']
+            for values in message_data:
+                name = values['name']
+                if name == 'From':
+                    from_name = values['value']
+                if name == 'Subject':
+                    subject = values['value']
+            if 'parts' in msg['payload']:        
+                msg_str = base64.urlsafe_b64decode(msg['payload']['parts'][0]['body']['data'].encode('ASCII')).decode('utf-8')
+                soup = BeautifulSoup(msg_str, 'html.parser')
+                body = soup.get_text()
+                print(f"From: {from_name}")
+                print(f"Subject: {subject}")
+            else:
+                print("noooooo") 
+
+
+def delete_last_message_from_sender(sender_name):
+    """
+    Retrieve emails from Gmail inbox and delete the last message from a specific sender
+    """
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    # Call the Gmail API to retrieve emails
+    results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        return 'No messages found.'
+
+    last_message_id = None
+    for message in messages:
+        msg = service.users().messages().get(userId='me', id=message['id']).execute()
+        message_data = msg['payload']['headers']
+        for values in message_data:
+            name = values['name']
+          
+            if name == 'From':
+                from_name = values['value']
+                print(from_name)
+                # Check if the sender matches the specified sender name
+                if from_name.strip().lower() == sender_name.strip().lower():
+
+                    last_message_id = message['id']
+                    break
+
+    if last_message_id:
+        # Delete the last message from the sender
+        service.users().messages().delete(userId='me', id=last_message_id).execute()
+        return f"Last message from {sender_name} deleted successfully!"
+    else:
+        return f"No messages found from {sender_name}."
+
+
+@app.route('/send')
+def send_email():
+    """
+    Send email using Gmail API
+    """
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    emailMsg = 'You won kkjhjkh'
+    mimeMessage = MIMEMultipart()
+    mimeMessage['to'] = 'vmail456345@gmail.com'
+    mimeMessage['subject'] = 'You weree'
+    mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+    raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+
+    message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+    return 'Email sent successfully!'
+
+@app.route('/read')
+
+def read():
+    resultnew = read_emails()
+    return resultnew
+
+@app.route('/') 
+      
+def delete():
+    sender_name = "newsletters@audible.in"
+    result = delete_last_message_from_sender(sender_name)
+    return result
+
+
+
+
+if __name__ == '__main__':
+    app.run()
